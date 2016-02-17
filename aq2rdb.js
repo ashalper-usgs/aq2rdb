c4f9d36dbe536613c6c21eb3eb7bc43f70b5810e
Andrew Halper <ashalper@usgs.gov>
2016-02-17 16:25:06 -0700

    Re-factored some functions into an "aquarius.js" module.

--- aq2rdb.js
+++ aq2rdb.js
@@ -24,8 +24,9 @@ var fs = require('fs');
 var moment = require('moment-timezone');
 
 // aq2rdb modules
-var rdbHeader = require('./rdbHeader');
+var aquarius = require('./aquarius');
 var fdvrdbout = require('./fdvrdbout');
+var rdbHeader = require('./rdbHeader');
 var site = require('./site');
 
 /**
@@ -33,12 +34,6 @@ var site = require('./site');
                 ".js" suffix.
 */
 var packageName = path.basename(process.argv[1]).slice(0, -3);
-
-/**
-   @description AQUARIUS Web services path prefix.
-   @constant
-*/
-var AQUARIUS_PREFIX = '/AQUARIUS/Publish/V2/';
 
 /**
    @description Domain of supported command line arguments.
@@ -632,123 +627,6 @@ function docRequest(url, name, response, callback) {
 } // docRequest
 
 /**
-   @function Call AQUARIUS GetTimeSeriesCorrectedData Web service.
-   @private
-   @param {string} token AQUARIUS authentication token.
-   @param {string} timeSeriesUniqueId AQUARIUS
-          GetTimeSeriesCorrectedData service
-          TimeSeriesDataCorrectedServiceRequest.TimeSeriesUniqueId
-          parameter.
-   @param {string} queryFrom AQUARIUS GetTimeSeriesCorrectedData
-          service TimeSeriesDataCorrectedServiceRequest.QueryFrom
-          parameter.
-   @param {string} QueryTo AQUARIUS GetTimeSeriesCorrectedData
-          service TimeSeriesDataCorrectedServiceRequest.QueryTo
-          parameter.
-   @param {function} callback Callback to call if/when
-          GetTimeSeriesCorrectedData service responds.
-*/
-function getTimeSeriesCorrectedData(
-    token, timeSeriesUniqueId, queryFrom, queryTo, callback
-) {
-    /**
-       @description Handle response from GetTimeSeriesCorrectedData.
-       @callback
-    */
-    function getTimeSeriesCorrectedDataCallback(response) {
-        var messageBody = '';
-        var timeSeriesCorrectedData;
-
-        // accumulate response
-        response.on(
-            'data',
-            function (chunk) {
-                messageBody += chunk;
-            });
-
-        response.on('end', function () {
-            callback(null, messageBody);
-            return;
-        });
-    } // getTimeSeriesCorrectedDataCallback
-
-    var path = AQUARIUS_PREFIX + 'GetTimeSeriesCorrectedData?' +
-        querystring.stringify(
-            {token: token, format: 'json',
-             TimeSeriesUniqueId: timeSeriesUniqueId,
-             QueryFrom: queryFrom, QueryTo: queryTo}
-        );
-
-    var request = http.request({
-        host: options.aquariusHostname,
-        path: path
-    }, getTimeSeriesCorrectedDataCallback);
-
-    /**
-       @description Handle GetTimeSeriesCorrectedData service
-                    invocation errors.
-    */
-    request.on('error', function (error) {
-        callback(error);
-        return;
-    });
-
-    request.end();
-} // getTimeSeriesCorrectedData
-
-/**
-   @function Call AQUARIUS GetLocationData Web service.
-   @private
-   @param {string} token AQUARIUS authentication token.
-   @param {string} locationIdentifier AQUARIUS location identifier.
-   @param {function} callback Callback function to call if/when
-          response from GetLocationData is received.
-*/
-function getLocationData(token, locationIdentifier, callback) {
-    /**
-       @description Handle response from GetLocationData.
-       @callback
-    */
-    function getLocationDataCallback(response) {
-        var messageBody = '';
-
-        // accumulate response
-        response.on(
-            'data',
-            function (chunk) {
-                messageBody += chunk;
-            });
-
-        response.on('end', function () {
-            callback(null, messageBody);
-            return;
-        });
-    }
-    
-    var path = AQUARIUS_PREFIX + 'GetLocationData?' +
-        querystring.stringify(
-            {token: token, format: 'json',
-             LocationIdentifier: locationIdentifier}
-        );
-
-    var request = http.request({
-        host: options.aquariusHostname,
-        path: path                
-    }, getLocationDataCallback);
-
-    /**
-       @description Handle GetTimeSeriesDescriptionList service
-                    invocation errors.
-    */
-    request.on('error', function (error) {
-        callback(error);
-        return;
-    });
-
-    request.end();
-} // getLocationData
-
-/**
    @function Create RDB, DV table row.
    @private
    @param {string} timestamp AQUARIUS timestamp string.
@@ -852,118 +730,6 @@ function dvTableRow(timestamp, value, qualifiers, remarkCodes, qa) {
 } // dvTableRow
 
 /**
-   @function Distill a set of time series descriptions into
-             (hopefully) one to query for a set of time series
-             date/value pairs.
-   @private
-   @param {object} timeSeriesDescriptions An array of AQUARIUS
-                   TimeSeriesDescription objects.
-   @param {object} locationIdentifier A LocationIdentifier object.
-   @param {function} callback Callback function to call if/when
-                     one-and-only-one candidate TimeSeriesDescription
-                     object is found, or, to call with node-async,
-                     raise error convention.
-*/
-function distill(timeSeriesDescriptions, locationIdentifier, callback) {
-    var timeSeriesDescription;
-
-    switch (timeSeriesDescriptions.length) {
-    case 0:
-        /**
-           @todo error callback
-        */
-        break;
-    case 1:
-        timeSeriesDescription = timeSeriesDescriptions[0];
-        break;
-    default:
-        /**
-           @description Filter out set of primary time series.
-        */
-        async.filter(
-            timeSeriesDescriptions,
-            /**
-               @function Primary time series filter iterator function.
-               @callback
-            */
-            function (timeSeriesDescription, callback) {
-                /**
-                   @description Detect
-                                {"Name": "PRIMARY_FLAG",
-                                 "Value": "Primary"} in
-                                 TimeSeriesDescription.ExtendedAttributes
-                */
-                async.detect(
-                    timeSeriesDescription.ExtendedAttributes,
-                    /**
-                       @function Primary time series, async.detect
-                                 truth value function.
-                       @callback
-                    */
-                    function (extendedAttribute, callback) {
-                        // if this time series description is
-                        // (hopefully) the (only) primary one
-                        if (extendedAttribute.Name === 'PRIMARY_FLAG'
-                            &&
-                            extendedAttribute.Value === 'Primary') {
-                            callback(true);
-                        }
-                        else {
-                            callback(false);
-                        }
-                    },
-                    /**
-                       @function Primary time series, async.detect
-                                 final function.
-                       @callback
-                    */
-                    function (result) {
-                        // notify async.filter that we...
-                        if (result === undefined) {
-                            // ...did not find a primary time series
-                            // description
-                            callback(false);
-                        }
-                        else {
-                            // ...found a primary time series
-                            // description
-                            callback(true);
-                        }
-                    }
-                );
-            },
-            /**
-               @function Check arity of primary time series
-                         descriptions returned from AQUARIUS
-                         GetTimeSeriesDescriptionList.
-               @callback
-            */
-            function (primaryTimeSeriesDescriptions) {
-                // if there is 1-and-only-1 primary time
-                // series description
-                if (primaryTimeSeriesDescriptions.length === 1) {
-                    timeSeriesDescription = timeSeriesDescriptions[0];
-                }
-                else {
-                    /**
-                       @todo We should probably defer production of
-                             header and heading until after this
-                             check.
-                    */
-                    // raise error
-                    callback(
-                        'More than 1 primary time series found for "' +
-                            locationIdentifier.toString() + '"'
-                    );
-                }
-            }
-        ); // async.filter
-    } // switch (timeSeriesDescriptions.length)
-
-    return timeSeriesDescription;
-} // distill
-
-/**
    @function Patch up some obscure incompatibilities between NWIS's
              site time offset predicate and IANA time zone data (used
              by moment-timezone).
@@ -1003,25 +769,6 @@ function nwisVersusIANA(timestamp, name, tzCode, localTimeFlag) {
 
     return p;
 } // nwisVersusIANA
-
-/**
-   @function Parse AQUARIUS TimeSeriesDataServiceResponse received
-             from GetTimeSeriesCorrectedData service.
-   @private
-*/
-function parseTimeSeriesDataServiceResponse(messageBody, callback) {
-    var timeSeriesDataServiceResponse;
-
-    try {
-        timeSeriesDataServiceResponse = JSON.parse(messageBody);
-    }
-    catch (error) {
-        callback(error);
-        return;
-    }
-
-    callback(null, timeSeriesDataServiceResponse);
-} // parsetimeSeriesDataServiceResponse
 
 /**
    @description GetDVTable endpoint service request handler.
@@ -1148,7 +895,7 @@ httpdispatcher.onGet(
                 try {
                     httpQuery(
                         options.aquariusHostname,
-                        AQUARIUS_PREFIX + 'GetTimeSeriesDescriptionList',
+                        aquarius.PREFIX + 'GetTimeSeriesDescriptionList',
                         obj,
                         callback
                     );
@@ -1223,14 +970,14 @@ httpdispatcher.onGet(
                         );
                         callback(null);
                     },
-		    /**
+                    /**
                        @function Write RDB header to HTTP response.
                        @callback
                     */
-		    function (callback) {
-			rdbHeader(response);
-			callback(null);
-		    },
+                    function (callback) {
+                        rdbHeader(response);
+                        callback(null);
+                    },
                     /**
                        @function Write RDB body to HTTP response.
                        @callback
@@ -1281,7 +1028,7 @@ httpdispatcher.onGet(
                 try {
                     httpQuery(
                         options.aquariusHostname,
-                        AQUARIUS_PREFIX + 'GetQualifierList/',
+                        aquarius.PREFIX + 'GetQualifierList/',
                         {token: token,
                          format: 'json'}, callback
                     );
@@ -1311,7 +1058,7 @@ httpdispatcher.onGet(
                 if (qualifierListServiceResponse === undefined) {
                     callback(
                         'Could not get remark codes from http://' +
-                            options.aquariusHostname + AQUARIUS_PREFIX +
+                            options.aquariusHostname + aquarius.PREFIX +
                             'GetQualifierList/'
                     );
                     return;
@@ -1522,7 +1269,7 @@ httpdispatcher.onGet(
                 try {
                     httpQuery(
                         options.aquariusHostname,
-                        AQUARIUS_PREFIX + 'GetTimeSeriesDescriptionList',
+                        aquarius.PREFIX + 'GetTimeSeriesDescriptionList',
                         {token: token, format: 'json',
                          LocationIdentifier: locationIdentifier.toString(),
                          Parameter: field.Parameter,
@@ -1593,16 +1340,16 @@ httpdispatcher.onGet(
                 );
                 callback(null, timeSeriesDescription);
             },
-	    /**
+            /**
                @function Write RDB header to HTTP response.
                @callback
             */
-	    function (callback) {
-		rdbHeader(response);
-		callback(null);
-	    },
+            function (callback) {
+                rdbHeader(response);
+                callback(null);
+            },
             /**
-	       @function Write RDB header body to HTTP response.
+               @function Write RDB header body to HTTP response.
                @callback
             */
             function (timeSeriesDescription, callback) {
