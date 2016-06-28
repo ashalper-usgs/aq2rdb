bac8a601d89b299f6b770e04fa5e1500f054fbb0
ashalper <ashalper@usgs.gov>
2016-06-28 14:50:53 -0700

    Factored-out extra-module callbacks.

--- usgs.js
+++ usgs.js
@@ -81,8 +81,6 @@ Site: function (locationIdentiferString) {
    @param {string} password NWIS-RA account password.
    @param {boolean} log Enable aq2rdb server logging if true; no
           logging if false.
-   @param {function} callback Callback function to call when
-          construction complete.
 */
 NWISRA: function (host, userName, password, log) {
     var authentication;
@@ -184,58 +182,13 @@ NWISRA: function (host, userName, password, log) {
        @param {string} path Path to Web service endpoint.
        @param {object} obj Web service REST parameters.
        @param {boolean} log Enable logging if true; no logging otherwise.
-       @callback {function} Callback function to call when query complete.
      */
-    function queryRemote(path, obj, log, callback) {
-        /**
-           @description Handle response from HTTPS query.
-           @callback
-        */
-        function queryCallback(response) {
-            var messageBody = '';
-
-            // accumulate response
-            response.on(
-                'data',
-                function (chunk) {
-                    messageBody += chunk;
-                });
-
-            response.on('end', function () {
-                if (
-                    response.statusCode < 200 || 300 <= response.statuscode
-                ) {
-                    callback(
-                        "Received an error from http://" + host +
-                            path + "; HTTP status code was: " +
-                            response.statusCode.toString()
-                    );
-                    return;
-                }
-                callback(null, messageBody);
-                return;
-            });
-        } // queryCallback
-
-        var request = https.request({
-            host: host,
-            method: "GET",
-            headers: {"Authorization": "Bearer " + authentication.tokenId},
-            path: path + '?' + querystring.stringify(obj)
-        }, queryCallback);
-
-        /**
-           @description Handle service invocation errors.
-        */
-        request.on("error", function (error) {
-            if (log)
-                console.log("service.NWISRA.queryRemote: " + error);
-            callback(error);
-            return;
-        });
-
-        request.end();
-
+    function queryRemote(path, obj, log) {
+        return rest.query(
+            "https", host, "GET",
+            {"Authorization": "Bearer " + authentication.tokenId},
+            path, obj, log
+        );
     } // queryRemote
 
     /**
@@ -245,39 +198,37 @@ NWISRA: function (host, userName, password, log) {
        @param {object} obj HTTP query parameter/value object.
        @param {boolean} log Enable console logging if true; no console
                         logging when false.
-       @param {function} callback Callback function to call if/when
-                         response is received.
     */
-    this.query = function (obj, log, callback) {
-        // if NWIS-RA is logged-into
-        if (authentication) {
-            queryRemote(
-                "/service/data/view/parameters/json",
-                obj,
-                log,
-                callback
-            );
-        }
-        else {
-            // emulate the query by referring to local copy of JSON data
-            var parameter = parameters.find(function (record) {
-                if (obj["parameters.PARM_CD"] === record.PARM_CD)
-                    return true;
-                else
-                    return false;
-            });
-
-            if (parameter === undefined) {
-                callback(
-                    "Could not find a corresponding AQUARIUS " +
-                        "Parameter name for USGS parameter code \"" +
-                        record.PARM_CD + "\""
-                );
-                return;
+    this.query = function (obj, log) {
+        return new Promise((resolve, reject) => {
+            // if NWIS-RA is logged-into
+            if (authentication) {
+                queryRemote(
+                    "/service/data/view/parameters/json", obj, log
+                )
+                    .then((messageBody) => {resolve(messageBody);});
             }
+            else {
+                // emulate the query by referring to local copy of JSON data
+                var parameter = parameters.find(function (record) {
+                    if (obj["parameters.PARM_CD"] === record.PARM_CD)
+                        return true;
+                    else
+                        return false;
+                });
 
-            callback(parameter);
-        }
+                if (parameter === undefined) {
+                    reject(
+                        'Could not find a corresponding AQUARIUS ' +
+                            'Parameter name for USGS parameter code "' +
+                            record.PARM_CD + '"'
+                    );
+                    return;
+                }
+
+                resolve(parameter);
+            }
+        });
     } // query
 
     // constructor
